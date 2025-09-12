7b1fb58a004efae7b77f78d4fe4c649b83e250c6
Rurutia1027 <rurutia1027@gmail.com>
2025-09-12 22:27:57 +0800

    update: refactor codes

--- example-app/src/main/java/com/aston/handson/controller/S3FileController.java
+++ example-app/src/main/java/com/aston/handson/controller/S3FileController.java
@@ -3,14 +3,8 @@ package com.aston.handson.controller;
 import com.aston.handson.core.S3ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/s3")
